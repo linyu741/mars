141615672a8ee4c506d58256ee1aae051a99fc3a
linyu741 <1185754617@qq.com>
2020-03-09 11:46:40 +0800

    1

--- stop.sh
+++ stop.sh
@@ -1,8 +1,5 @@
-#!/bin/sh
-
-NAME="mars-reporting-web.jar"
-
-
-PID=$(cat /home/cyberarkservice/mars-reporting/"$NAME".pid)
-kill -9 $PID
-
+#查找该端口的进程信息,并将第二列的进程号赋给ID
+ID=`lsof -i:8080 |grep java |awk '{print $1}'`
+echo $ID
+kill -9 $ID
+echo "kill success!"
