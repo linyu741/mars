09086cfdf9d144bc7f4cc8703c968915e5581511
linxinglong <123@1234>
2020-04-28 10:44:17 +0800

    2020年4月28日10:44:11 UAT

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ echo '2'
 sleep 1
 echo '1'
 sleep 1
-nohup java -jar mars-reporting-web.jar & 
+nohup java -Xms1024m -Xmx1024m  -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/data/app/cot/heapdump.hprof -jar /usr/local/cotpss/CotPssService-0.0.1-SNAPSHOT-exec.jar --spring.profiles.active=dev &
